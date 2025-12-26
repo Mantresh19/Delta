486143c5a46ae1ec4242175ec136436bb0354279
Liladhar <mantresh19@gmail.com>
2025-12-26 13:03:02 +0000

    Arithematic Operators

--- JavaScript/JavaScript/index.js
+++ JavaScript/JavaScript/index.js
@@ -3,3 +3,15 @@ let pencilprice = 10;
 let erasorprice = 5;
 // let output = "The total price is: " + (pencilprice + erasorprice) + "£".
 console.log(`The total price is : ${pencilprice + erasorprice} Rupees.`);
+
+//Arithematic operators
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(++a);

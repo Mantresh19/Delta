a07a21e6f273a6c9a964c8673bfb70efc26d9512
Liladhar <mantresh19@gmail.com>
2025-12-29 11:24:29 +0000

    Remove commented-out Twitter post object literal from part5.js

--- JavaScript/JavaScript/part5.js
+++ JavaScript/JavaScript/part5.js
@@ -6,10 +6,21 @@
 // };
 
 // Twitter post
-let post = {
-    Username: 'Mantresh',
-    Content: 'Coding',
-    Likes: 200,
-    Repost: 53,
-    Tags: 3
+// let post = {
+//     Username: 'Mantresh',
+//     Content: 'Coding',
+//     Likes: 200,
+//     Repost: 53,
+//     Tags: 3
+// };
+
+let classinfo = {
+    Mantresh: {
+        Grade: 'A+',
+        City: 'Nashik'
+    },
+    Vky: {
+        Grade: '-D',
+        City: 'Nashik'
+    }
 };

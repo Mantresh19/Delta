0904003b418094873dfcb8e7fdd879c0bd197c76
Liladhar <mantresh19@gmail.com>
2026-01-08 16:31:40 +0000

    Refactor partt11.js: replace promise-based fetch calls with async/await for improved readability and error handling

--- JavaScript/JavaScript/partt11.js
+++ JavaScript/JavaScript/partt11.js
@@ -166,21 +166,37 @@
 // console.log(JSON.stringify(student))
 
 let url = 'https://catfact.ninja/fact';
-fetch(url)
-.then ((res) => {
-    console.log(res);
-    return res.json();
-})
-.then((data) => {
-    console.log('Data1 = ', data.fact)
-    return fetch(url)
-})
-.then((res) => {
-    return res.json();
-})
-.then((data2) => {
-    console.log("Data2 = ", data2.fact)
-})
-.catch((err) => {
-    console.log(err)
-})
+async function getFacts() {
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+        
+    } catch (err) {
+        console.log('catch err', err);
+    }
+    console.log('bye');
+}
+// fetch(url)
+// .then ((res) => {
+//     console.log(res);
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log('Data1 = ', data.fact)
+//     return fetch(url)
+// })
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data2) => {
+//     console.log("Data2 = ", data2.fact)
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
+

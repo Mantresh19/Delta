76329c3aa8aad4731fa65d4bd4eb58a8ee972b85
Liladhar <mantresh19@gmail.com>
2026-01-10 19:19:33 +0000

    HTML and CSS of Password Checker

--- Practice/index1.js
+++ Practice/index1.js
@@ -1,49 +1,51 @@
-const countEl = document.getElementById("count");
-const messageEl = document.getElementById('message');
-const increaseBtn = document.getElementById("increase");
-const resetBtn = document.getElementById("reset");
-const decreaseBtn = document.getElementById("decrease");
+// Smart Counter
+// const countEl = document.getElementById("count");
+// const messageEl = document.getElementById('message');
+// const increaseBtn = document.getElementById("increase");
+// const resetBtn = document.getElementById("reset");
+// const decreaseBtn = document.getElementById("decrease");
 
-let count = 0;
+// let count = 0;
 
-const updateUI = () => {
-    countEl.textContent = count;
+// const updateUI = () => {
+//     countEl.textContent = count;
 
-    if (count === 0) {
-        countEl.classList.add("red");
-        countEl.classList.remove("green");
-    } else {
-        countEl.classList.add("green");
-        countEl.classList.remove("red");
-    }
+//     if (count === 0) {
+//         countEl.classList.add("red");
+//         countEl.classList.remove("green");
+//     } else {
+//         countEl.classList.add("green");
+//         countEl.classList.remove("red");
+//     }
 
-    decreaseBtn.disabled = count === 0;
-    increaseBtn.disabled = count === 10;
+//     decreaseBtn.disabled = count === 0;
+//     increaseBtn.disabled = count === 10;
 
-    if (count === 10) {
-        messageEl.textContent = 'Maximum Limit Reached';
-    } else if (count === 0) {
-        messageEl.textContent = 'Minimum Limit Reached';
-    } else messageEl.textContent = '';
-};
+//     if (count === 10) {
+//         messageEl.textContent = 'Maximum Limit Reached';
+//     } else if (count === 0) {
+//         messageEl.textContent = 'Minimum Limit Reached';
+//     } else messageEl.textContent = '';
+// };
 
-increaseBtn.addEventListener("click", () => {
-    if (count < 10) {
-        count++;
-        updateUI();
-    }
-});
+// increaseBtn.addEventListener("click", () => {
+//     if (count < 10) {
+//         count++;
+//         updateUI();
+//     }
+// });
 
-decreaseBtn.addEventListener("click", () => {
-    if (count > 0){
-        count--;
-        updateUI();
-    }
-});
+// decreaseBtn.addEventListener("click", () => {
+//     if (count > 0){
+//         count--;
+//         updateUI();
+//     }
+// });
 
-resetBtn.addEventListener("click", () => {
-    count = 0;
-    updateUI();
-})
+// resetBtn.addEventListener("click", () => {
+//     count = 0;
+//     updateUI();
+// })
 
-updateUI();
+// updateUI();
+

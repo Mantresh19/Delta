df554e72bc038d879b6354cf680900d0326eef67
Liladhar <mantresh19@gmail.com>
2026-01-12 23:29:40 +0000

    Node JS export

---
+++ Practice/part1.js
@@ -0,0 +1,18 @@
+Logo
+
+
+Search-bar
+
+
+Central icons
+<i class="fa-regular fa-house nav-center-icon"></i>
+                <i class="fa-solid fa-people-group nav-center-icon"></i>
+                <i class="fa-solid fa-video nav-center-icon"></i>
+                <i class="fa-solid fa-store nav-center-icon"></i>
+                <i class="fa-solid fa-address-card nav-center-icon"></i>
+
+Right icons
+<i class="fa-solid fa-cubes nav-right-icon"></i>
+<i class="fa-brands fa-facebook-messenger nav-right-icon"></i>
+<i class="fa-solid fa-bell nav-right-icon"></i>
+<i class="fa-solid fa-user nav-right-icon nav-right-user"></i>

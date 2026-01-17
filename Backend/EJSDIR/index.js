87faf04691837a35c5fc8a232b1b6332875ba0ed
Liladhar <mantresh19@gmail.com>
2026-01-17 12:18:53 +0000

    Add new views and routes for dice roll and Instagram profile
    switch server port to 8080 and set EJS views directory
    add /rolldice and /ig/:username routes
    introduce rolldice.ejs and instagram.ejs templates

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -1,10 +1,12 @@
 // Important code
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const port = 3000;
+const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"))
 
 app.get("/", (req, res) => {
     res.render("view.ejs")
@@ -12,4 +14,14 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening to port: ${port}`);
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceval = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice", {diceval});
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    res.render("instagram.ejs", {username});
 });

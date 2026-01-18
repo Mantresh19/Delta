25ed3626239d47133d8a8bbe932944d3e7caffd8
Liladhar <mantresh19@gmail.com>
2026-01-18 22:27:39 +0000

    !

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded ({extended: true}))
+app.use(express.json())
+
 app.get("/register", (req, res) => {
     let {user, password} = req.query;
     res.send(`Standard Get response. Welcome ${user}`)
 })
 
 app.post("/register", (req, res) => {
-    res.send("Standard Post response")
+    let {user, password} = req.body;
+    res.send(`Standard Post response. Welcome ${user}`)
 })
 
 app.listen(port, () => {

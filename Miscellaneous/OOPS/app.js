355e33359974e43b59572fdf495c7338cdb65aac
Liladhar <mantresh19@gmail.com>
2026-01-18 23:02:56 +0000

    OOPS

---
+++ Miscellaneous/OOPS/app.js
@@ -0,0 +1,51 @@
+// Constructors
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+// }
+
+// Person.prototype.talk = function() {
+//     console.log(`Hi, my name is ${this.name}`)
+// }
+
+// here "h" is a pot-hole and Ramesh, Pakya, Charan are potholes on the roads
+// let h1 = new Person("Ramesh", 2)
+// let h2 = new Person("Pakya", 3)
+// let h3 = new Person("Charan", 3)
+
+// Classes
+// class Personn {
+//     constructor(namee, agee) {
+//         this.namee = namee
+//         this.agee = agee
+//     }
+//     speak() {
+//         console.log(`Hi my name is ${namee}`)
+//     }
+// }
+// let p1 = new Personn("Mantresh", 20)
+
+// Inheritance
+// class Main {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//     talk() {
+//         console.log(`Hi, my name is ${name}`)
+//     }
+// }
+
+// class Student extends Main {
+//     constructor(name, age, marks) {
+//         super(name, age)
+//         this.marks = marks
+//     }
+// }
+
+// class Teacher extends Main{
+//     constructor(name, age, subject) {
+//         super(name, age)
+//         this.subject = subject
+//     }
+// }

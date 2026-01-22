c4c781701e93a1486e1cc46f5efd6f7182e13cd5
Liladhar <mantresh19@gmail.com>
2026-01-22 11:25:05 +0000

    Installed UUID and fixed some errors on Rest

--- Backend/Rest/index.js
+++ Backend/Rest/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 
 app.use(express.urlencoded({ extended: true}));
 
@@ -45,9 +47,9 @@ app.post("/posts", (req, res)=> {
 
 app.get("/posts/:id", (req, res)=> {
     let {id} = req.params
-    let post = posts.find((p) => id === p.id);
-    res.render("show.ejs")
-    res.send("request working")
+    let post = posts.find((p) => id === p.id)
+     if (!post) return res.status(404).send("Post not found");
+    res.render("show.ejs", { post });
 })
 
 app.listen(port, () => {

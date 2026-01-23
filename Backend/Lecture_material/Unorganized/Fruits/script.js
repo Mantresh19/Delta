3f648b35ff57875887c59eb8be6551282c3b75df
Liladhar <mantresh19@gmail.com>
2026-01-23 23:49:55 +0000

    !

---
+++ Backend/Lecture_material/Unorganized/Fruits/script.js
@@ -0,0 +1,10 @@
+// Fruits
+// const info = require("./Fruits");
+
+// console.log(info);
+
+
+// Import
+import { generate, count } from "random-words";
+console.log(generate());
+console.log(count());

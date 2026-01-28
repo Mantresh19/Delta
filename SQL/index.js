b773b83826366942005fe518cc2b384ac8275aa8
Liladhar <mantresh19@gmail.com>
2026-01-28 12:15:28 +0000

    err

--- SQL/index.js
+++ SQL/index.js
@@ -1,14 +1,9 @@
-import {faker} from '@faker-js/faker'
-// Get the client
-import mysql from 'mysql2/promise'
+import { faker } from "@faker-js/faker";
+import mysql from "mysql2/promise";
+import express from "express";
 
-// Create the connection to database
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'delta_app',
-  password: 'SDE@f66ng'
-});
+const app = express();
+const port = 8080;
 
 // Insert random data
 let getRandomUser = () => {
@@ -20,38 +15,44 @@ let getRandomUser = () => {
   };
 };
 
-console.log(getRandomUser());
+// const q = "INSERT INTO `user` (id, username, email, password) VALUES ?";
 
-let q = "INSERT INTO `user` (id, username, email, password) VALUES ?";
-
-let data = [];
-for (let i=1; i<=100; i++) {
-   const user = getRandomUser();
-   data.push([user.id, user.username, user.email, user.password]);
-}
-
-// Test 
-try {
-  const [result] = await connection.query(q, [data]);
-  console.log("Inserted:", result.affectedRows);
-  } catch (err) {
-    console.log(err);
-} finally {
-  await connection.end();
-}
-
-// Working method
-// try {
-//   const [rows] = await connection.query("SHOW TABLES");
-//   console.log(rows);
-//   } catch (err) {
-//     console.log(err)
-// } finally {
-//   await connection.end();
+// const data = [];
+// for (let i = 1; i <= 100; i++) {
+//   const user = getRandomUser();
+//   data.push([user.id, user.username, user.email, user.password]);
 // }
 
-//  connection.end();
+async function main() {
+  // Create the connection to database
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "delta_app",
+    password: "SDE@f66ng",
+  });
 
+app.get("/", async(req, res) => {
+  let q = `SELECT count(*) FROM user`;
+  try {
+      const [result] = await connection.query(q, [data]);
+      console.log("Inserted:", result.affectedRows);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await connection.end();
+      res.send("App is working")
+    }
+  }
+)};
+
+main().catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log("Listening to port 8080");
+});
+
+// Faker
 // let getRandomUser= () => {
 //   return {
 //     userId: faker.string.uuid(),

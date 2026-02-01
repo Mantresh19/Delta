b919f695767121df2c7ad334b3a40f37c31789bd
Liladhar <mantresh19@gmail.com>
2026-02-01 14:43:19 +0000

    Add mongoose CRUD examples and Book model
    Mongodb/books.js introduces Book schema/model and sample insert
    Mongodb/index.js adds deleteMany example plus commented CRUD snippets

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -21,3 +21,36 @@ const userSchema = mongoose.Schema({
 // Models is a mongoose class which we use to construct documents
 
 const User = mongoose.model("User", userSchema);
+
+User.deleteMany({name: "Charam"}, {name: "Pakya"})
+.then((res) => console.log(res))
+.catch((err) => console.log(err))
+
+// User.findOneAndUpdate({name: "Ramesh"}, {name: "Pinku"}, {new: true})
+// .then((res)=>console.log(res))
+// .catch((err)=> console.log(err))
+
+// User.findById("697ea28d2cbd005ee128221d")
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
+
+// Inserting multiple data
+// User.insertMany([
+//     {name: "Ramesh", emai: "ramesh@bxr.ac.uk", age: 5},
+//     {name: "Pakya", emai: "pakya@bxr.ac.uk", age: 3},
+//     {name: "Charam", emai: "charan@bxr.ac.uk", age: 2},
+// ]).then((res) => {
+//     console.log(res)
+// }).catch((err) => console.log(err))
+
+// Inserting single data
+// const user2 = new User({
+//     name: "eve",
+//     email: "eve@gmail.co.uk",
+//     age: 11
+// })
+
+// user2.save().then((res) => {
+//     console.log(res)
+// })
+// .catch((err) => console.log(err))

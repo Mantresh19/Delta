bc4a593584790af9b20bb092ff02ed75408e96ef
Liladhar <mantresh19@gmail.com>
2026-02-01 00:28:41 +0000

    Forming connection

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+.then(() => {
+    console.log("Connection successful!!");
+})
+.catch((err) => console.log(err))
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}

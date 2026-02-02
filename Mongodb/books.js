c4a6d68567b6e0bdb266fda09ff907c1c440ed05
Liladhar <mantresh19@gmail.com>
2026-02-02 18:54:07 +0000

    !

--- Mongodb/books.js
+++ Mongodb/books.js
@@ -13,33 +13,39 @@ async function main() {
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
-        maxLength: 10
+        required: true
     },
     author: {
         type: String
     },
     price: {
         type: Number,
-        min: 1
+        min: [1, "Price is too low for amazon selling"]
     },
     discount: {
         type: Number,
         default: 0
+    },
+    category: {
+        type: String,
+        enum: ["fiction", "non-fiction"]
     }
 })
 
 const Book = mongoose.model("Book", bookSchema)
 
-
-let book1 = new Book({
-    title: "laws of power",
-    author: "robert gree",
-    price: 1110,
-    discount
-})
-
-book1.save()
+Book.findByIdAndUpdate("6980d517ba47d82217b6f290", { price: -100}, {runValidators: true})
 .then((res) => console.log(res))
-.catch((err) => console.log(err))
+.catch((err) => console.log(err.errors.price.properties.message))
+
+// let book1 = new Book({
+//     title: "laws of power",
+//     author: "robert gree",
+//     price: 1110,
+//     category: "fiction"
+// })
+
+// book1.save()
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
 

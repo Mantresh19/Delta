a267a25f9b00df977ccaaa580bc2866957c08f4b
Liladhar <mantresh19@gmail.com>
2026-02-08 19:48:55 +0000

    CC

---
+++ Projects/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 8080;
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+
+main().then(() => {
+    console.log("Connect to DB")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+async function main() {
+    await mongoose.connect(MONGO_URL)
+}
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`listening to port: ${port}`);
+});

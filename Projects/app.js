25a1a0520ce297f5a941ceb29e7a73be07727757
Liladhar <mantresh19@gmail.com>
2026-02-10 23:47:07 +0000

    cc

--- Projects/app.js
+++ Projects/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
+const Listing = require("./models/listing")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 
@@ -18,6 +19,19 @@ async function main() {
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root")
+})
+
+app.get("/testlisting", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 12000,
+        location: "Kokan",
+        country: "India"
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successful testing")
 })
 
 app.listen(port, (req, res) => {
--- Projects/models/listing.js
+++ Projects/models/listing.js
@@ -7,11 +7,15 @@ const listingSchema = new Schema({
         required: true
     },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg",
+        set: (v) => v === "" ? "https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg" : v
+    },
     price: Number,
     location: String,
     country: String
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;

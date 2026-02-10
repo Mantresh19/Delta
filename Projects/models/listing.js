466f917b7d4ce0541473ee8f70ae114bb153776b
Liladhar <mantresh19@gmail.com>
2026-02-10 11:55:43 +0000

    Schema

--- Projects/models/listing.js
+++ Projects/models/listing.js
@@ -1,1 +1,17 @@
-12345678
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: String,
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+modules.export = Listing;

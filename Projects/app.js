19c43bba8aea848c7c2a3f55c37a2715a42caf52
Liladhar <mantresh19@gmail.com>
2026-02-12 22:03:56 +0000

    Add EJS views for listings index and show pages

--- Projects/app.js
+++ Projects/app.js
@@ -18,13 +18,25 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root")
 })
 
+// Index route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("index.ejs", {allListings})
+    res.render("listings/index.ejs", {allListings})
+})
+
+//Show Route
+app.get("/listings/:id", async(req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/show.ejs", {listing})
 })
 
 // app.get("/testlisting", async (req, res) => {

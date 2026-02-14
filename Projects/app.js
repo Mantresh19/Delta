a1c84c2783d2bbb7df29a124cb6e8d4c2b9fb32e
Liladhar <mantresh19@gmail.com>
2026-02-14 18:56:34 +0000

    Refactor listing creation form and route to handle nested object structure

--- Projects/app.js
+++ Projects/app.js
@@ -46,7 +46,9 @@ app.get("/listings/:id", async(req, res) => {
 
 // Create Route
 app.post("/listings", async (req, res) => {
-    
+    const newListing = new Listing(req.body.listing)
+    await newListing.save()
+    res.redirect("/listings")
 })
 
 // app.get("/testlisting", async (req, res) => {

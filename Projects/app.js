41686e700fcd6565d97125d0624794f3c1cb5a4c
Liladhar <mantresh19@gmail.com>
2026-02-16 22:18:15 +0000

    feat: implement layout structure with boilerplate, navbar, and footer; add static file serving

--- Projects/app.js
+++ Projects/app.js
@@ -25,6 +25,7 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({extended: true}))
 app.use(methodoverride("_method"))
 app.engine('ejs', ejsMate)
+app.use(express.static(path.join(__dirname, "views/public")))
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root")

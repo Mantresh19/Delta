f8fe8996dd2431e526f24392b95e2e231e69ddeb
Liladhar <mantresh19@gmail.com>
2026-04-09 17:11:55 +0100

    Refactor addData function to retrieve existing user and simplify post creation

--- Relationships/Models/posts.js
+++ Relationships/Models/posts.js
@@ -27,20 +27,15 @@ const User = mongoose.model("User", userSchema);
 const Post = mongoose.model("Post", postSchema);
 
 const addData = async () => {
-    let user1 = new User({
-        username: "rahulkumar",
-        email: "rahulkumar69@gmail.com"
+    let user = await User.find({username: "rahulkumar"});
+
+    let post2 = new Post({
+        content: "Bye bye",
+        Likes: 71
     })
 
-    let post1 = new Post({
-        content: "Hello World",
-        Likes: 7
-    })
-
-    post1.user = user1;
-
-    await user1.save();
-    await post1.save();
+    post2.user = user;
+    await post2.save();
 }
 
 addData();

97b22b3e9ccca6f9062e2efc331f4c971bea245c
Liladhar <mantresh19@gmail.com>
2026-08-25 20:37:54 +0100

    Add /getcookies endpoint to send cookies in response

--- Projects/classroom/server.js
+++ Projects/classroom/server.js
@@ -3,10 +3,15 @@ const app = express();
 const users = require("./routes/user")
 const posts = require("./routes/post")
 
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "Hello");
+    res.cookie("Namaste", "India");
+    res.send("Sent you some cookies!");
+})
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root!");
 })
-
 app.use("/users", users)
 app.use("/posts", posts)
 

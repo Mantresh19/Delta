10ad1b0ba1e3b2486e3007f7bbcf440e8189d9e4
Liladhar <mantresh19@gmail.com>
2026-09-15 21:40:31 +0100

    feat: add safe-buffer and uid-safe modules
    - Introduced safe-buffer module with TypeScript definitions and implementation for a safer Buffer API.
    - Added uid-safe module for generating URL and cookie safe UIDs with both asynchronous and synchronous methods.
    - Included necessary package.json files for both modules with appropriate metadata and dependencies.
    - Added documentation and history files for uid-safe module.

--- Projects/classroom/server.js
+++ Projects/classroom/server.js
@@ -2,32 +2,40 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user")
 const posts = require("./routes/post")
-const cookieParser = require("cookie-parser")
+const session = require("express-session") 
 
-app.use(cookieParser("secretcode"))
+app.use(session({secret: "supersecretcookie"}))
 
-app.get("/getcookies", (req, res) => {
-    res.cookie("greet", "Hello");
-    res.cookie("Namaste", "India");
-    res.send("Sent you some cookies!");
+app.get("/test", (req, res) => {
+    res.send("Test successful")
 })
 
-app.get("/getsignedcookies", (req, res) => {
-    res.cookie("made-in", "India", {signed: true})
-    res.send("Signed cookie sent")
-})
+// const cookieParser = require("cookie-parser")
 
-app.get("/verify", (req, res) => {
-    console.log(req.cookies)
-    res.send("verified")
-})
+// app.use(cookieParser("secretcode"))
 
-app.get("/", (req, res) => {
-    console.dir(req.cookies)
-    res.send("Hi, I am root!");
-})
-app.use("/users", users)
-app.use("/posts", posts)
+// app.get("/getcookies", (req, res) => {
+//     res.cookie("greet", "Hello");
+//     res.cookie("Namaste", "India");
+//     res.send("Sent you some cookies!");
+// })
+
+// app.get("/getsignedcookies", (req, res) => {
+//     res.cookie("made-in", "India", {signed: true})
+//     res.send("Signed cookie sent")
+// })
+
+// app.get("/verify", (req, res) => {
+//     console.log(req.cookies)
+//     res.send("verified")
+// })
+
+// app.get("/", (req, res) => {
+//     console.dir(req.cookies)
+//     res.send("Hi, I am root!");
+// })
+// app.use("/users", users)
+// app.use("/posts", posts)
 
 app.listen(3000, () => {
     console.log("server is listening to 3000")
